14bad9a3fa4f0f6da90a980e2c3c0a330ed4e5eb
Mr. Joe <radics.pepe@gmail.com>
2022-11-25 20:13:50 +0100

    hamburgermenű_1_save_11.25

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let Hbutton = document.getElementById("js-button");
+let menu = document.querySelector("#menu-list");
+let Hicon = document.getElementById("button-img");
+
+Hbutton.addEventListener("click", hmenuOpen);
+
+function hmenuOpen() {
+  console.log("teszt");
+  menu.classList.toggle("active");
+  if (menu.classList == "active") {
+    Hicon.src = "./img/icon/close.svg";
+  } else {
+    Hicon.src = "./img/icon/list.svg";
+  }
+}
