5c14325350a4562e1831f6cbdd5bb6c3a292886c
Mr. Joe <radics.pepe@gmail.com>
2022-12-10 12:20:37 +0100

    elkzedtem a portfoliohoz jst írni

--- js/main.js
+++ js/main.js
@@ -1,15 +1,46 @@
+//--------------------------------------Hamburger menu---------------------------------------
+
+//Variables
 let Hbutton = document.getElementById("js-button");
 let menu = document.querySelector("#menu-list");
 let Hicon = document.getElementById("button-img");
 
+//Events
 Hbutton.addEventListener("click", hmenuOpen);
+menu.addEventListener("click", hmenuOpen);
 
+//Funciton
+
+//This function activet the nav menu
 function hmenuOpen() {
-  console.log("teszt");
   menu.classList.toggle("active");
+  //This if replaces the button icon
   if (menu.classList == "active") {
     Hicon.src = "./img/icon/close.svg";
   } else {
     Hicon.src = "./img/icon/list.svg";
   }
+}
+
+//--------------------------------------Portfolio---------------------------------------
+
+//Variables
+let portButton = document.querySelectorAll(".portfolio-button");
+let table = document.querySelectorAll(".tableX");
+
+for (let i = 0; i < portButton.length; i++) {
+  portButton[i].addEventListener("click", function () {
+    for (let j = 0; j < portButton.length; j++) {
+      portButton[j].classList.remove("active-button");
+    }
+    this.classList.add("active-button");
+  });
+}
+
+//Events
+
+//Funciton
+
+function proba() {
+  console.log("I Love for cik! ;)");
 }
